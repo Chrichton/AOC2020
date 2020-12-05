58e1d5e7630a0932aabc2446165c1afbfc2597e7
Heiko Goes <heiko.goes@gaebler-goes.de>
2020-12-05 13:41:30 +0100

    solve with input calculates wrong result 99

--- day3/lib/day3.ex
+++ day3/lib/day3.ex
@@ -4,15 +4,23 @@ defmodule Day3 do
   """
 
   def solve1() do
-    File.read!("testinput")
+    File.read!("input")
     |> String.split("\n")
     |> count_trees_on_way()
   end
 
-  def count_trees_on_way(map, position \\ {0, 0}) do
+  def count_trees_on_way(map) do
+    max_y = Enum.count(map) - 1
+
+    count_trees_on_way_recursive(map, {0, 0}, max_y, 0)
+  end
+
+  def count_trees_on_way_recursive(_, {_, y}, max_y, acc) when y > max_y, do: acc
+
+  def count_trees_on_way_recursive(map, position, max_y, acc) do
     if char_at_position(map, position) == "#",
-      do: 1 + count_trees_on_way(map, next_position(position)),
-      else: count_trees_on_way(map, next_position(position))
+      do: count_trees_on_way_recursive(map, next_position(position), max_y, acc + 1),
+      else: count_trees_on_way_recursive(map, next_position(position), max_y, acc)
   end
 
   def next_position({x, y}), do: {x + 3, y + 1}
--- day3/test/day3_test.exs
+++ day3/test/day3_test.exs
@@ -2,31 +2,21 @@ defmodule Day3Test do
   use ExUnit.Case
   doctest Day3
 
-  @testinput """
-  ..##.......
-  #...#...#..
-  .#....#..#.
-  ..#.#...#.#
-  .#...##..#.
-  ..#.##.....
-  .#.#.#....#
-  .#........#
-  #.##...#...
-  #...##....#
-  .#..#...#.#
-  """
-
-  def map(), do: String.split(@testinput, "\n")
+  def map(), do: File.read!("testinput") |> String.split("\n")
 
   test "char_at_position" do
-    assert Day3.char_at_position(map, {1, 2}) == "#"
+    assert Day3.char_at_position(map(), {1, 2}) == "#"
   end
 
   test "char_at_position x out of range" do
-    assert Day3.char_at_position(map, {13, 2}) == "#"
+    assert Day3.char_at_position(map(), {13, 2}) == "#"
   end
 
   test "solve1" do
-    Day3.count_trees_on_way(map()) == 7
+    assert Day3.count_trees_on_way(map()) == 2
+  end
+
+  test "solve1 input" do
+    assert Day3.solve1() == 99
   end
 end
