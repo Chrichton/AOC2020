46820f307a85fc8b9a536354dfba1c19c425c075
Heiko Goes <heiko.goes@gaebler-goes.de>
2020-12-07 21:48:21 +0100

    multiplication is missing

--- day7/lib/day7.ex
+++ day7/lib/day7.ex
@@ -3,6 +3,8 @@ defmodule Day7 do
   Documentation for `Day7`.
   """
 
+  @shiny_gold_bag "shiny gold bag"
+
   def solve1() do
     File.read!("input")
     |> String.split("\n")
@@ -11,7 +13,7 @@ defmodule Day7 do
 
   def collect(string_list) do
     string_list
-    |> Enum.filter(&(not String.starts_with?(&1, "shiny gold bag")))
+    |> Enum.filter(&(not String.starts_with?(&1, @shiny_gold_bag)))
     |> find_shiny_gold_bag_containers()
     |> replace_occurrences(string_list)
     |> find_shiny_gold_bag_containers()
@@ -38,7 +40,7 @@ defmodule Day7 do
 
   def find_shiny_gold_bag_containers(string_list) do
     Enum.reduce(string_list, [], fn string, acc ->
-      if String.contains?(string, "shiny gold bag"),
+      if String.contains?(string, @shiny_gold_bag),
         do: [first_three_words_without_last_character(string) | acc],
         else: acc
     end)
@@ -47,13 +49,13 @@ defmodule Day7 do
   def find_shiny_gold_bag_occurrences(string_list) do
     Enum.filter(string_list, fn string ->
       String.slice(string, 1, String.length(string))
-      |> String.contains?("shiny gold bag")
+      |> String.contains?(@shiny_gold_bag)
     end)
   end
 
   def replace_occurrences(replace_strings, string_list) do
     Enum.map(string_list, fn string ->
-      if String.contains?(string, "shiny gold bag") do
+      if String.contains?(string, @shiny_gold_bag) do
         string
       else
         Enum.reduce(replace_strings, string, fn replace_string, acc ->
@@ -61,7 +63,7 @@ defmodule Day7 do
           string_without_first_char = String.slice(acc, 1, String.length(acc))
 
           first_char <>
-            String.replace(string_without_first_char, replace_string, "shiny gold bag")
+            String.replace(string_without_first_char, replace_string, @shiny_gold_bag)
         end)
       end
     end)
@@ -86,12 +88,42 @@ defmodule Day7 do
     |> calculate_contained_bags()
   end
 
-  def calculate_contained_bags(string_list) do
-    [shiny_gold_bag_contents] =
-      Enum.filter(string_list, &String.starts_with?(&1, "shiny gold bags contain"))
+  def calculate_contained_bags(bags) do
+    calculate_contained_bags_recursive(bags, to_name_quantity_list(bags, @shiny_gold_bag), [])
   end
 
-  def parse_bag(bags, bag_name) do
+  def calculate_contained_bags_recursive(bags, names_quantities, acc) do
+    acc = acc ++ names_quantities
+
+    current_names_quantities =
+      names_quantities
+      |> Enum.flat_map(fn {name, _} -> to_name_quantity_list(bags, name) end)
+      |> Enum.filter(fn {_, quantity} -> quantity > 0 end)
+
+    if Enum.count(current_names_quantities) == 0 do
+      acc
+    else
+      current_names_quantities = update_current_names_quantities(current_names_quantities, acc)
+
+      calculate_contained_bags_recursive(
+        bags,
+        current_names_quantities,
+        acc
+      )
+    end
+  end
+
+  def update_current_names_quantities(current_names_quantities, acc) do
+    Enum.map(current_names_quantities, fn {name, quantity} ->
+      case Enum.find(acc, fn {acc_name, _} -> acc_name == name end) do
+        {_, acc_quantity} -> {name, quantity * acc_quantity}
+        _ -> {name, quantity}
+      end
+    end)
+  end
+
+  #  :: [{name, quantity}]
+  def to_name_quantity_list(bags, bag_name) do
     [bag] = Enum.filter(bags, &String.starts_with?(&1, bag_name))
 
     String.split(bag, " ")
@@ -99,6 +131,7 @@ defmodule Day7 do
     |> Enum.join(" ")
     |> String.trim()
     |> String.split(",")
+    |> Enum.filter(&(not String.contains?(&1, @shiny_gold_bag)))
     |> Enum.map(fn bag_line ->
       components =
         bag_line
@@ -106,9 +139,9 @@ defmodule Day7 do
         |> String.split(" ")
 
       quantity =
-        components
-        |> Enum.at(0)
-        |> String.to_integer()
+        if Enum.at(components, 0) == "no",
+          do: 0,
+          else: String.to_integer(Enum.at(components, 0))
 
       name =
         components
--- day7/test/day7_test.exs
+++ day7/test/day7_test.exs
@@ -2,15 +2,19 @@ defmodule Day7Test do
   use ExUnit.Case
   doctest Day7
 
-  test "first_three_words_without_last_character" do
-    assert Day7.first_three_words_without_last_character("Hallo Du da wie geht's") == "Hallo Du d"
-  end
+  # test "first_three_words_without_last_character" do
+  #   assert Day7.first_three_words_without_last_character("Hallo Du da wie geht's") == "Hallo Du d"
+  # end
 
-  test "solve testinput" do
-    assert File.read!("testinput") |> String.split("\n") |> Day7.collect() == 4
-  end
+  # test "solve testinput" do
+  #   assert File.read!("testinput") |> String.split("\n") |> Day7.collect() == 4
+  # end
 
-  test "solve1" do
-    assert Day7.solve1() == 229
+  # test "solve1" do
+  #   assert Day7.solve1() == 229
+  # end
+
+  test "solve2" do
+    assert Day7.solve2() == 42
   end
 end
