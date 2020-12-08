c8ca6418c8c7c877133a94b3a98a52dcb7b543d3
Heiko Goes <heiko.goes@gaebler-goes.de>
2020-12-08 17:27:02 +0100

    solve2 with [number] instead of tuple

--- day1/lib/day1.ex
+++ day1/lib/day1.ex
@@ -27,33 +27,25 @@ defmodule Day1 do
   #------------------------------------
 
   def solve2() do
-    {number1, number2, number3} =
       File.read!("input")
       |> String.split("\n")
       |> Enum.map(&String.to_integer/1)
       |> find_triple_with_sum_2020()
-
-    number1 * number2 * number3
+      |> Enum.reduce(1, fn number, acc -> acc * number end)
   end
 
   def find_triple_with_sum_2020([number1 | numbers]) do
     case find_triple_with_sum_2020(number1, numbers) do
-      {:ok, {first_number, second_number, third_number}} -> {first_number, second_number, third_number}
+      {:ok, found_numbers} -> found_numbers
       _ -> find_triple_with_sum_2020(numbers)
     end
   end
 
   def find_triple_with_sum_2020(_number1, []), do: {:error, "not_found"}
   def find_triple_with_sum_2020(number1, [number2 | numbers]) do
-    case Enum.reduce_while(numbers, {:error, "not_found"}, fn x, _accu -> sum_2020?(number1, number2, x) end) do
-      {:ok, {first_number, second_number, third_number}} -> {:ok, {first_number, second_number, third_number}}
+    case Enum.reduce_while(numbers, {:error, "not_found"}, fn x, _accu -> sum_2020?([number1, number2, x]) end) do
+      {:ok, found_numbers} -> {:ok, found_numbers}
       _ -> find_triple_with_sum_2020(number1, numbers)
     end
-  end
-
-  def sum_2020?(number1, number2, number3) do
-    if number1 + number2 + number3 == 2020,
-      do: {:halt, {:ok, {number1, number2, number3}}},
-      else: {:cont, {:error, "not_found"}}
   end
 end
