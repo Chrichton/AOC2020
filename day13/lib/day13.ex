b9aca7a6db037464c18afcbfe797ee27314dd0e6
Heiko Goes <heiko.goes@gaebler-goes.de>
2020-12-13 08:37:16 +0100

    first try solve2

--- day13/lib/day13.ex
+++ day13/lib/day13.ex
@@ -37,4 +37,55 @@ defmodule Day13 do
 
     {current_time, ids}
   end
+
+  # -------------
+
+  def solve2() do
+    File.read!("testinput")
+    |> String.split("\n")
+    |> Enum.at(1)
+    |> parse_input2()
+    |> Enum.reverse()
+    |> find_coincidence()
+  end
+
+  def parse_input2(ids_string) do
+    ids_string
+    |> String.split(",")
+    |> Enum.reduce({[], 0}, fn char, {result, timestamp} ->
+      if char == "x" do
+        {result, timestamp + 1}
+      else
+        bus_id = String.to_integer(char)
+        {[{bus_id, timestamp} | result], timestamp + 1}
+      end
+    end)
+    |> elem(0)
+  end
+
+  def find_coincidence(pairs) do
+    factor =
+      pairs
+      |> Enum.at(0)
+      |> elem(0)
+
+    find_coincidence_recursive(pairs, factor)
+  end
+
+  def find_coincidence_recursive(pairs, timestamp) do
+    if check_coincidence(pairs, timestamp),
+      do: timestamp,
+      else: find_coincidence_recursive(pairs, timestamp + timestamp)
+  end
+
+  def check_coincidence(pairs, current_timestamp) do
+    pairs
+    |> Enum.map(fn {bus_id, timestamp} ->
+      {timestamp, get_nearest_departure_time(current_timestamp, bus_id)}
+    end)
+    |> Enum.filter(fn {timestamp, nearest_departure_time} ->
+      timestamp == nearest_departure_time
+    end)
+    |> Enum.count() == 1
+  end
 end
--- day13/test/day13_test.exs
+++ day13/test/day13_test.exs
@@ -2,11 +2,19 @@ defmodule Day13Test do
   use ExUnit.Case
   doctest Day13
 
-  test "parse_input" do
-    assert Day13.parse_input(["939", "7,13,x,x,59,x,31,19"]) == {939, [7, 13, 59, 31, 19]}
-  end
+  # test "parse_input" do
+  #   assert Day13.parse_input(["939", "7,13,x,x,59,x,31,19"]) == {939, [7, 13, 59, 31, 19]}
+  # end
 
-  test "solve1" do
-    assert Day13.solve1() == 295
+  # test "solve1" do
+  #   assert Day13.solve1() == 119
+  # end
+
+  # test "parse_input2" do
+  #   assert Day13.parse_input2("7,13,x,x,59,x,31,19") == [{19, 7}, {31, 6}, {59, 4}, {13, 1}, {7, 0}]
+  # end
+
+  test "solve2" do
+    assert Day13.solve2() == 295
   end
 end
