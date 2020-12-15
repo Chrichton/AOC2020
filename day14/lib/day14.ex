0c5efe21d5774c05e34a36421fd1580b2022238e
Heiko Goes <heiko.goes@gaebler-goes.de>
2020-12-15 18:25:26 +0100

    next step

--- day14/lib/day14.ex
+++ day14/lib/day14.ex
@@ -76,4 +76,99 @@ defmodule Day14 do
 
     {mem, value}
   end
+
+  # -------------
+
+  def solve2() do
+    File.read!("testinput2")
+    |> parse_input()
+    |> sum_memory_values2()
+  end
+
+  def sum_memory_values2(masks_values) do
+    Enum.reduce(masks_values, %{}, fn mask_values, acc ->
+      calc_memory_value2(mask_values, acc)
+    end)
+    |> Map.values()
+    |> Enum.sum()
+  end
+
+  # {mask, [{_mem_address, _value}]}
+  def calc_memory_value2({mask, values}, memory_map) do
+    Enum.reduce(values, memory_map, fn {mem_address, value}, acc ->
+      Map.put(acc, mem_address, calc_value2(mask, value))
+    end)
+  end
+
+  def calc_value2(mask, mem_value) do
+    binary_string =
+      mem_value
+      |> Integer.to_string(2)
+      |> String.pad_leading(String.length(mask), "0")
+
+    get_binary_strings(binary_string, mask)
+    |> Enum.map(fn binary_string ->
+      binary_string
+      |> to_char_list()
+      |> List.to_integer(2)
+    end)
+    |> Enum.sum()
+  end
+
+  def get_binary_strings(binary_string, mask) do
+    Enum.zip(
+      String.codepoints(mask),
+      String.codepoints(binary_string)
+    )
+    |> Enum.reduce("", fn {mask_char, value_char}, acc ->
+      if mask_char == "0",
+        do: acc <> value_char,
+        else: acc <> mask_char
+    end)
+    |> get_binary_strings()
+  end
+
+  def get_binary_strings(binary_string_with_x) do
+    codepoints = String.codepoints(binary_string_with_x)
+    x_count = Enum.count(codepoints, &(&1 == "X"))
+
+    Enum.map(floating_permutations(x_count), fn permutation ->
+      Enum.reduce(codepoints, {"", permutation, 0},
+      fn char, {result, permutation, permutation_index} ->
+        if char == "X",
+          do:
+            {result <> (Enum.at(permutation, permutation_index) |> Integer.to_string()), permutation,
+             permutation_index + 1},
+          else: {result <> char, permutation, permutation_index}
+      end)
+      |> elem(0)
+    end)
+  end
+
+  # 2 -> 0,0 0,1 1,0 1,1
+  # def permutations(number) do
+  #   for x <- 1..number, y <- 1..number, (x, y)
+  # end
+
+  def floating_permutations(number) do
+    Enum.map(0..(number - 1), & &1)
+    |> shuffle()
+    |> Enum.filter(fn list ->
+      Enum.all?(list, &(&1 <= 1))
+    end)
+  end
+
+  # def permutations([]), do: [[]]
+
+  # def permutations(list),
+  #   do: for(elem <- list, rest <- permutations(list -- [elem]), do: [elem | rest])
+
+  def shuffle(list), do: shuffle(list, length(list))
+
+  def shuffle([], _), do: [[]]
+  def shuffle(_, 0), do: [[]]
+
+  def shuffle(list, i) do
+    for x <- list, y <- shuffle(list, i - 1), do: [x | y]
+  end
 end
--- day14/test/day14_test.exs
+++ day14/test/day14_test.exs
@@ -2,38 +2,42 @@ defmodule Day14Test do
   use ExUnit.Case
   doctest Day14
 
-  test "parse_input" do
-    line = """
-    mask = X
-    mem1
-    mem2
-    mask = 0
-    mem3
-    mem4
-    """
+  # test "parse_input" do
+  #   line = """
+  #   mask = X
+  #   mem1
+  #   mem2
+  #   mask = 0
+  #   mem3
+  #   mem4
+  #   """
 
-    testinput = File.read!("testinput2")
+  #   testinput = File.read!("testinput2")
 
-    assert Day14.parse_input(testinput) == [
-             {"0X10110X1001000X10X00X01000X01X01101", [{49559, 97}]},
-             {"00XX1111100100XX1000X1X00001111X1010", [{16422, 941_878_948}]}
-           ]
-  end
+  #   assert Day14.parse_input(testinput) == [
+  #            {"0X10110X1001000X10X00X01000X01X01101", [{49559, 97}]},
+  #            {"00XX1111100100XX1000X1X00001111X1010", [{16422, 941_878_948}]}
+  #          ]
+  # end
 
-  test "calc value" do
-    Day14.calc_value("XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X", 11) == 73
-  end
+  # test "calc value" do
+  #   Day14.calc_value("XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X", 11) == 73
+  # end
 
-  test "solve1 testdata" do
-    result =
-      File.read!("testinput")
-      |> Day14.parse_input()
-      |> Day14.sum_memory_values()
+  # test "solve1 testdata" do
+  #   result =
+  #     File.read!("testinput")
+  #     |> Day14.parse_input()
+  #     |> Day14.sum_memory_values()
 
-      assert result == 165
-  end
+  #     assert result == 165
+  # end
 
-  test "solve1" do
-    assert Day14.solve1() == 7477696999511
+  # test "solve1" do
+  #   assert Day14.solve1() == 7477696999511
+  # end
+
+  test "solve2" do
+    assert Day14.solve2() == 7477696999511
   end
 end
