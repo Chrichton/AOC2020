667a53ac1a43bbc8fbb177e21fdbbaca3ea888a1
Heiko Goes <heiko.goes@gaebler-goes.de>
2020-12-17 05:48:05 +0100

    initial

---
+++ day17/lib/day17.ex
@@ -0,0 +1,10 @@
+defmodule Day17 do
+  @moduledoc """
+  Documentation for `Day17`.
+  """
+
+  def solve1() do
+    File.read!("input")
+    |> String.split("\n")
+  end
+end
---
+++ day17/test/day17_test.exs
@@ -0,0 +1,8 @@
+defmodule Day17Test do
+  use ExUnit.Case
+  doctest Day17
+
+  test "greets the world" do
+    assert Day17.solve1() == :world
+  end
+end
