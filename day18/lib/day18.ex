9da5d8cdda9b9db32004df6c4f558d7caab3806d
Heiko Goes <heiko.goes@gaebler-goes.de>
2020-12-21 16:19:24 +0100

    next step

--- day18/lib/day18.ex
+++ day18/lib/day18.ex
@@ -12,4 +12,61 @@ defmodule Day18 do
   def parse_input(lines) do
     lines
   end
+
+  def evaluate(line) do
+    line
+    |> String.codepoints()
+    |> Enum.zip(0..String.length(line) -1)
+    |> Enum.reduce({"", "", ""}, fn {char, index}, {operator, param1, param2} ->
+      case char do
+        " " ->
+          {operator, param1, param2}
+
+        "*" ->
+          if operator == "" do
+            {"*", param1, param2}
+          else
+            result = calculate({operator, param1, param2})
+            {"*", result, ""}
+          end
+
+        "+" ->
+          if operator == "" do
+            {"+", param1, param2}
+          else
+            result = calculate({operator, param1, param2})
+            {"+", result, ""}
+          end
+
+        "(" ->
+          rest = String.slice(line, index + 1, String.length(line))
+
+          end_index = index_of(rest, ")") - 1;
+          expression = String.slice(rest, 0, end_index + 1)
+          {operator, param1, evaluate(expression)}
+
+        param ->
+          if operator == "" do
+            {operator, param1 <> param, param2}
+          else
+            {operator, param1, param2 <> param}
+          end
+      end
+    end)
+    |> calculate()
+    |> String.to_integer()
+  end
+
+  def calculate({operator, param1, param2}) do
+    if operator == "+" do
+      (String.to_integer(param1) + String.to_integer(param2))
+      |> Integer.to_string()
+    else
+      (String.to_integer(param1) * String.to_integer(param2))
+      |> Integer.to_string()
+    end
+  end
+
+  def index_of(string, search_text), do:
+    :binary.match(string, search_text) |> elem(0)
 end
--- day18/test/day18_test.exs
+++ day18/test/day18_test.exs
@@ -2,7 +2,11 @@ defmodule Day18Test do
   use ExUnit.Case
   doctest Day18
 
-  test "solve1" do
-    assert Day18.solve1() == :world
+  # test "evaluate simple" do
+  #   assert Day18.evaluate("1 + 2 * 3 + 4 * 5 + 6") == 71
+  # end
+
+  test "evaluate" do
+    assert Day18.evaluate("1 + (2 * 3) + (4 * (5 + 6))") == 51
   end
 end
